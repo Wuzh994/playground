46dba0daab4ddd952351c7322f279de0eaf3a0db
wuzhenghong <2015129064@qq.com>
2022-11-09 22:47:43 +0800

    feat: add cesium

---
+++ src/components/MetaEarth.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { Viewer } from 'cesium'
+import { onMounted } from 'vue'
+
+onMounted(() => {
+  const viewer = new Viewer('cesiumContainer')
+})
+</script>
+
+<template>
+  <div id="cesiumContainer" />
+</template>
+
+<style scoped>
+html,
+body,
+#cesiumContainer {
+  width: 100%;
+  height: 100%;
+}
+</style>
